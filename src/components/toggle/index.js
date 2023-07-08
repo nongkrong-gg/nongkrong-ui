b3ffe872966401d1123108cc3ec6acbb9ff31085
Muhammad Denaw <daivasmara@gmail.com>
2023-07-09 02:12:10 +0700

    [FIX] Dark Mode Toggle

--- src/components/toggle/index.js
+++ src/components/toggle/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useContext } from 'react';
 import { DarkModeSwitch } from 'react-toggle-dark-mode';
 import { DarkModeContext } from 'contexts';
 
@@ -6,14 +6,15 @@ function Toggle() {
   const darkmode = useContext(DarkModeContext);
   const [isDarkMode, setDarkMode] = useState(darkmode.isActivated());
 
-  useEffect(() => {
+  const toggleDarkMode = () => {
     darkmode.toggle();
-  }, [isDarkMode]);
+    setDarkMode((prevValue) => !prevValue);
+  };
 
   return (
     <DarkModeSwitch
       checked={isDarkMode}
-      onChange={(checked) => setDarkMode(checked)}
+      onChange={toggleDarkMode}
       size={24}
       sunColor="#F2C94C"
     />
